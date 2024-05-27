0d433991faeab13cd2386c9b013d725b906fc367
Sergey Zholudev <serg.zholudev@gmail.com>
2024-05-27 14:41:44 +0300

    Folder of templates was moved to the project level. Settings was changed for correct work.

--- ownCity/ownCity/settings.py
+++ ownCity/ownCity/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -51,11 +52,12 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'ownCity.urls'
+ТЕМРLАТЕ_DIR = os.path.join(BASE_DIR, "templates")
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ТЕМРLАТЕ_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
